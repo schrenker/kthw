f02fe0452e354ba8a5e2b3c8448060e4e85523ce
schrenker <zawadzkis95@gmail.com>
2021-11-15 14:09:33 +0100

    template for ssh.cfg

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ resource "local_file" "inventory" {
   })
   filename = "./ansible/inventory"
 }
+
+resource "local_file" "sshcfg" {
+  content = templatefile("./templates/sshcfg.tmpl",
+    {
+      ansibleJumpbox = azurerm_linux_virtual_machine.Jumpbox.public_ip_address
+  })
+  filename = "./ansible/ssh.cfg"
+}
